faed98f563441310c0f06d535a75a28cf0e164fc
Trish201021 <100299339+Trish201021@users.noreply.github.com>
2022-04-13 11:06:51 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 100; index++) {
+        counter += 1
+        sum = counter + sum
+        basic.showNumber(sum)
+    }
+})
+let sum = 0
+let counter = 0
+counter += 0
+sum = 0
+basic.forever(function () {
+	
+})
